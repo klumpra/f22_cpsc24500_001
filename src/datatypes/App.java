8c2894734ac63a8702991297afde9e5f15ead707
klumpra <klumpra@lewisu.edu>
2022-09-09 12:39:25 -0500

    Added datatypes and funwithmath

---
+++ src/datatypes/App.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class App {
+	public static void main(String[] args) {
+		int luckyNumber = 7;
+		int unluckyNumber;
+		unluckyNumber = 13;
+		double value = 14.32;
+		float anotherValues = 5.67f;
+		double result = (double)luckyNumber/unluckyNumber;
+		System.out.println(result);
+	}
+}
---
+++ src/funwithmath/App.java
@@ -0,0 +1,13 @@
+package funwithmath;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
+		double cosine, sine;
+		cosine = Math.cos(Math.PI/3);
+		sine = Math.sin(Math.PI/3);
+		System.out.println(cosine);
+		System.out.println(sine);
+	}
+}
