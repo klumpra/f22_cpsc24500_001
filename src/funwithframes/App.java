1a6b8751dffcf75b8bb4cb2f073ebe4b4ec96e49
Ray Klump <klumpra@lewisu.edu>
2022-11-16 10:50:15 -0600

    First user interface example

---
+++ src/funwithframes/App.java
@@ -0,0 +1,7 @@
+package funwithframes;
+public class App {
+	public static void main(String[] args) {
+		MyFrame frm = new MyFrame();
+		frm.setVisible(true);
+	}
+}
---
+++ src/funwithframes/MyFrame.java
@@ -0,0 +1,10 @@
+package funwithframes;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		setTitle("My first frame");
+		setBounds(100,50,500,500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+}
