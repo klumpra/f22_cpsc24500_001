d9d687622fb7de828e186a76b1d56f568d771f1f
Ray Klump <klumpra@lewisu.edu>
2022-11-30 10:50:47 -0600

    Added ability to respond to button being clicked.

--- src/uiwithmvc20221128/ShapeFrame.java
+++ src/uiwithmvc20221128/ShapeFrame.java
@@ -2,23 +2,52 @@ package uiwithmvc20221128;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
-public class ShapeFrame extends JFrame {
+public class ShapeFrame extends JFrame /*implements ActionListener*/ {
+	private ArrayList<Shape> shapes;
+	private ShapePanel panDrawing;
+	private ShapeMover mover;
 	public void setupGUI(ArrayList<Shape> shapes) {
+		this.shapes = shapes;
+		mover = new ShapeMover(shapes);
 		setTitle("Drawing Frame");
 		setBounds(100,100,500,500);
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
 		JButton btnOK = new JButton("OK");
+//		ButtonHandler bh = new ButtonHandler();
+//		btnOK.addActionListener(bh);
+		btnOK.addActionListener(
+			new ActionListener() { // spontaneously creating a class with no name that implements ActionListener
+				public void actionPerformed(ActionEvent e) {
+//					JOptionPane.showMessageDialog(null, "Anonymous inner class approach.");
+/*					for (Shape s : shapes) {
+						s.setX(s.getX()+5);
+						s.setY(s.getY()+5);
+					}
+*/
+					mover.moveRandomly();
+					repaint();  //tells the frame to repaint itself - tells each component inside to repaint too
+				}
+			}
+		);
 		c.add(btnOK,BorderLayout.SOUTH);
-		ShapePanel panDrawing = new ShapePanel(shapes);
+		panDrawing = new ShapePanel(shapes);
 		c.add(panDrawing,BorderLayout.CENTER);
 	}
+	/*
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null,"It is only Wednesday.");
+	}
+	*/
 	public ShapeFrame(ArrayList<Shape> shapes) {
 		setupGUI(shapes);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
--- src/uiwithmvc20221128/ShapePanel.java
+++ src/uiwithmvc20221128/ShapePanel.java
@@ -16,27 +16,6 @@ public class ShapePanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		/*
-		int count = 1;
-		for (Shape s : shapes) {
-			g.drawString(s.toString(), 50, count*50);
-			count = count + 1;
-		}
-		*/
-		String type;
-		int x, y, radius, width, length;
-		for (Shape s : shapes) {
-			type = s.getType();
-			x = s.getX();
-			y = s.getY();
-			if (type.equals("circle")) {
-				radius = ((Circle)s).getRadius();
-				g.drawOval(x, y, 2*radius, 2*radius);
-			} else if (type.equals("rectangle")) {
-				width = ((Rectangle)s).getWidth();
-				length = ((Rectangle)s).getLength();
-				g.drawRect(x,y,width,length);
-			}
-		}
+		ShapeDrawer.drawShapes(shapes, g);
 	}
 }
